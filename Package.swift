acfea16613b7c6f9b6998ee2c9a202a44e797ac2
Ramon Torres <raymondjavaxx@gmail.com>
2023-07-10 17:19:42 -0400

    Setup CI (#4)
    * Add lint workflow
    
    * Add Makefile
    
    * Add build workflow
    
    * Cleanup Package.swift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
-// swift-tools-version: 5.8
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version: 5.7
 
 import PackageDescription
 
@@ -11,23 +10,18 @@ let package = Package(
         .macOS(.v11)
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "ColorToolbox",
-            targets: ["ColorToolbox"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+            targets: ["ColorToolbox"]
+        )
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "ColorToolbox",
-            dependencies: []),
+            name: "ColorToolbox"
+        ),
         .testTarget(
             name: "ColorToolboxTests",
-            dependencies: ["ColorToolbox"]),
+            dependencies: ["ColorToolbox"]
+        )
     ]
 )
