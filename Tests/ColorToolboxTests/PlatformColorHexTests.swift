d57d42fc9a154c709b7f9a0a6d32cbea09374e6b
Ramon Torres <raymondjavaxx@gmail.com>
2023-07-16 13:17:59 -0400

    Additional cases

--- Tests/ColorToolboxTests/PlatformColorHexTests.swift
+++ Tests/ColorToolboxTests/PlatformColorHexTests.swift
@@ -40,6 +40,22 @@ final class PlatformColorHexTests: XCTestCase {
 
     func test_hex_convertingBackAndForth() throws {
         let webColors: [String] = [
+            "#000000",
+            "#C0C0C0",
+            "#808080",
+            "#FFFFFF",
+            "#800000",
+            "#FF0000",
+            "#800080",
+            "#FF00FF",
+            "#008000",
+            "#00FF00",
+            "#808000",
+            "#FFFF00",
+            "#000080",
+            "#0000FF",
+            "#008080",
+            "#00FFFF",
             "#F0F8FF",
             "#FAEBD7",
             "#00FFFF",
